9406a2bb4712d5b2dc740c66e4a7536b59ae9b70
Kennu156 <kenmarttipaju@gmail.com>
2024-09-25 14:06:21 +0300

    commut

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,24 @@
 <script setup>
+import { onMounted, ref } from 'vue';
+import axios from 'axios'
+
+const products = ref([])
+
+const fetchAllProducts = async () => {
+  const response = await axios.get('https://fakestoreapi.com/products');
+  return response.data
+};
+
+onMounted(async () => await fetchAllProducts())
 </script>
 
 <template>
   <h1>Welcome to fake store</h1>
+  <div class="grid grid-cols-4 gap-4">
+    <article v-for="product in products" :key="product.id">Toote info
+    <img :src="product.image" alt=""><h4>{{ product.title }}</h4>
+    </article>  
+  </div>
+  
 </template>
 
